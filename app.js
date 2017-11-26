937064452780ac687eaec839ee8f393f9b3bc14e
Esther Liao <erliao@princeton.edu>
2017-11-26 21:00:28 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
+app.get('/', function(req, resp){
+  const html = `
+    <!doctype html>
+    <html>
+      <head>
+        <title>Let's Use EJS</title>
+      </head>
+      <body>
+        <h1>Learning To Use EJS</h1>
+      </body>
+    </html>
+  `
 
+  resp.send(html)
+})
 
 module.exports = app

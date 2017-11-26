8a97e41dc816d1d4c2147ab8443b5e3bc4ec1134
Esther Liao <erliao@princeton.edu>
2017-11-26 21:01:16 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
+const path = require('./views/index');
 app.get('/', function(req, resp){
   const html = `
     <!doctype html>
